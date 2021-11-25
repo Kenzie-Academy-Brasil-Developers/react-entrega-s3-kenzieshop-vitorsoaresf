19e85705081e0889537316fcfe3edc91f40170d1
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-25 08:00:04 -0300

    feat: implements sync localstorage-reduxthunk

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,10 +1,17 @@
 import { useSelector } from "react-redux";
 import { Container } from "./styles";
 import Product from "../../components/Product";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
-  const cart = useSelector((state) => state.cart);
-  console.log(cart);
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem("@cart")));
+  console.log("aqui", cart);
+  const cartUpdate = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("@cart")));
+  }, [cartUpdate]);
+
   return (
     <Container>
       {cart.map((product, index) => (
