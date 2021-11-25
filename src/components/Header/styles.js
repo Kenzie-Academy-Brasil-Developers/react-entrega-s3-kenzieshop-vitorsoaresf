f3f909568285b42696b6cb5367f57566b5fe27ef
vitorsoaresf <vitorsoaresf@gmail.com>
2021-11-25 12:21:54 -0300

    feat: implements style header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,24 +2,38 @@ import styled from "styled-components";
 
 export const Container = styled.header`
   width: 100vw;
-  height: 50px;
+  height: 70px;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  border-bottom: 1px solid var(--grey-50);
 
   h1 {
+    font-size: 1.5rem;
     margin-left: 10px;
   }
+
   div {
     display: flex;
     button {
-      background-color: wheat;
       width: 50px;
-      font-size: 1.2rem;
+      font-size: 1rem;
     }
+
     button + button {
       margin-left: 10px;
       margin-right: 10px;
+    }
+  }
+
+  @media (min-width: 1024px) {
+    h1 {
+      font-size: 1.8rem;
+    }
+    div {
+      button {
+        font-size: 1.3rem;
+      }
     }
   }
 `;
